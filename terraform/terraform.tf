1e1cfa421e3af6056b413d947620ae7eb63b0b3c
Rytis Sileika <rts.pub@pulegium.eu>
2023-07-09 13:22:40 +0100

    setup backen and provider

---
+++ terraform/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.5"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.7"
+    }
+  }
+
+  backend "s3" {
+    bucket = "rsi-tf-nomad-2023-state"
+    region = "us-east-2"
+    key = "terraform.tfstate"
+  }
+}

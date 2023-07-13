68b6a236fab35339f80bc6a1c0dbc143ed0ea7da
Rytis Sileika <rts.pub@pulegium.eu>
2023-07-13 16:30:57 +0100

    move ssh out in to a module

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,20 +24,6 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "tls_private_key" "ssh_key" {
-  algorithm = "ED25519"
-}
-
-resource "aws_key_pair" "nomad" {
-  key_name = "nomad"
-  public_key = tls_private_key.ssh_key.public_key_openssh
-}
-
-resource "local_sensitive_file" "nomad_ssh_key" {
-  content = "${tls_private_key.ssh_key.private_key_openssh}"
-  filename = "${path.module}/nomad_ssh.key"
-}
-
 module "nomad_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/nomad"
   version = "~> 5.0"
@@ -56,6 +42,10 @@ module "ssh_security_group" {
   vpc_id = module.vpc.vpc_id
 }
 
+module "ssh_key" {
+  source = "./modules/ssh"
+}
+
 module "nomad_public" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
@@ -64,7 +54,7 @@ module "nomad_public" {
   ami = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
-  key_name = aws_key_pair.nomad.key_name
+  key_name = module.ssh_key.key_name
   subnet_id = module.vpc.public_subnets[count.index]
   vpc_security_group_ids = [
     module.nomad_security_group.security_group_id,
